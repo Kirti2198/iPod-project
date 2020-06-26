8aa97b4f01b253d2f8e6f019280b2e6f7ae4e4cd
Kirti2198 <kirti2198@gmail.com>
2020-06-26 13:30:05 +0530

    made ipod and wheel component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Ipod from './ipod';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Ipod-project
-      </header>
+      <Ipod />
     </div>
   );
 }
---
+++ src/ipod.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Wheel from './wheel';
+
+class Ipod extends React.Component{
+    // constructor(){
+
+    // }
+    render(){
+        return(
+        <h1>Ipod</h1>,
+        <Wheel />)
+        ;
+    }
+}
+
+export default Ipod;
